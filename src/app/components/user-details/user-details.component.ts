e71f6340ce01c5ebdc7e068bec93b4b9a141d226
ReyhanPA <reyhanputra95@gmail.com>
2025-06-09 17:20:35 +0700

    feat: update components with table and details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from '../../models/user';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  styleUrl: './user-details.component.css',
 })
 export class UserDetailsComponent {
+  user: User | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+
+    if (idParam !== null) {
+      const id = Number(idParam);
+      this.userService.GetUserByID(id).subscribe((user) => (
+        this.user = user
+      ));
+    }
+  }
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.GetUsers().subscribe((users) => (
+      this.users = users
+    ));
+  }
 }
