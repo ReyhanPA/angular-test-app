63e99c096d3890d2b0fe1643a66fa608fb8edc67
ReyhanPA <reyhanputra95@gmail.com>
2025-06-09 22:31:10 +0700

    feat: update table with paginator, search, and sort

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,23 +1,97 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/user';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    RouterModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormField,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+  ],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
-export class UserListComponent implements OnInit {
-  users: User[] = [];
+export class UserListComponent implements OnInit, AfterViewInit {
+  displayedColumns = [
+    'name',
+    'email',
+    'website',
+    'city',
+    'companyName',
+    'action',
+  ];
+  dataSource = new MatTableDataSource<User>();
+  filterText = '';
 
-  constructor(private userService: UserService) {}
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+  @ViewChild(MatSort) sort?: MatSort;
+
+  constructor(
+    private userService: UserService,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
 
   ngOnInit(): void {
-    this.userService.GetUsers().subscribe((users) => (
-      this.users = users
-    ));
+    this.userService
+      .GetUsers()
+      .subscribe((users) => (this.dataSource.data = users));
+  }
+
+  ngAfterViewInit(): void {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        switch (property) {
+          case 'city':
+            return item.address?.city || '';
+          case 'companyName':
+            return item.company?.name || '';
+          default:
+            return item[property as keyof User] as string;
+        }
+      };
+    }
+  }
+
+  announceSortChange(sortState: Sort): void {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  applyFilter(): void {
+    this.dataSource.filter = this.filterText.trim().toLowerCase();
+  }
+
+  clearFilter(): void {
+    this.filterText = '';
+    this.applyFilter();
   }
 }
