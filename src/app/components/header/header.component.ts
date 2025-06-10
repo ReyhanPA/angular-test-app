3be4334e1cb15d8a4d4e0a3517a52e99b37f31c5
ReyhanPA <reyhanputra95@gmail.com>
2025-06-10 13:50:55 +0700

    feat: add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MenuItem } from '../../models/menu-item';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  title = 'Angular Test App';
-  menu: MenuItem[] = [
-    { name: 'Users', path: '/' }
-  ];
-}
+export class HeaderComponent {}
