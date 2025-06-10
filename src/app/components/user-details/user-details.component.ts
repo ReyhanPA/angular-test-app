048777f9fc76bb147ceecfc7e82d2e29c3a76e42
ReyhanPA <reyhanputra95@gmail.com>
2025-06-10 13:51:34 +0700

    feat: update style responsive details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,18 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { User } from '../../models/user';
-import { ActivatedRoute } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css',
 })
 export class UserDetailsComponent {
   user: User | null = null;
+  isLoading = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,9 +36,19 @@ export class UserDetailsComponent {
 
     if (idParam !== null) {
       const id = Number(idParam);
-      this.userService.getUserByID(id).subscribe((user) => (
-        this.user = user
-      ));
+      this.userService.getUserByID(id).subscribe({
+        next: (user) => {
+          this.user = user;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error fetching user', err);
+          this.isLoading = false;
+        },
+      });
+    } else {
+      
+      this.isLoading = false;
     }
   }
 }
